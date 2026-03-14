60e3ab0835ba6420a9eb5b5c380f7e4fc95fc795
aniks4399 <anirudha670@gmail.com>
2026-03-14 21:40:29 +0530

    Remove unused boto3 import
    Removed unused import for boto3.

--- Advanced_python_project/Corp_bonds_metrics.py
+++ Advanced_python_project/Corp_bonds_metrics.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import boto3
 import os
 from dotenv import load_dotenv
 from sklearn.model_selection import train_test_split  
