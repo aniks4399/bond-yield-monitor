6b1c69144443806c407f1af034fdece3c096f151
aniks4399 <anirudha670@gmail.com>
2026-03-14 21:39:33 +0530

    Remove unused sqlite3 import
    Removed unused sqlite3 import from Corp_bonds_metrics.py

--- Advanced_python_project/Corp_bonds_metrics.py
+++ Advanced_python_project/Corp_bonds_metrics.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import boto3
 import os
 from dotenv import load_dotenv
-import sqlite3
 from sklearn.model_selection import train_test_split  
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
