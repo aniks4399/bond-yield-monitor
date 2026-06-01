61e3aafd6e230e158911d66cafb0df33f7e2ec7e
aniks4399 <anirudha670@gmail.com>
2026-06-01 20:08:55 +0530

    Refactor code for fetching and processing FRED data

--- fred.py
+++ fred.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import boto3
 import os
 from dotenv import load_dotenv
 
@@ -17,24 +16,9 @@ data = response.json()
 df=pd.DataFrame(data['observations'])
 df.head()
 df1=df.copy()
-#df1=df1[df1['value'] != '.']
-#df1=df1[['date','value']]
-df1=df1.loc[df1['value']!='.',['date','value']]
-df2=df1.to_csv('bonds_data.csv', index=False)
-bd=pd.read_csv('bonds_data.csv')
-bd.head()
-AWS_ACCESS_KEY_ID = os.environ.get("ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = os.environ.get("SECRET_KEY")
-s3=boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-)
+
+df1.loc[df1['value'] != '.', ['date', 'value']]
+df2 = df1.to_csv('bonds_data.csv', index=False)
+bd = pd.read_csv('bonds_data.csv')
+print(bd.head())
 print(df1)
-print("Connection is successful!")
-# Upload the file
-# Syntax: s3.upload_file(Source_File, Bucket_Name, Cloud_File_Name)
-
-s3.upload_file('bonds_data.csv', 'bonds-data-anirudha-4399', 'bonds_data_cloud.csv')
-
-print("Upload successful! Check your AWS Console.")     
